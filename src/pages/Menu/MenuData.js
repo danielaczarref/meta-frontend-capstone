e76f6e7ff55226e841c22a5785b14b14adb42a54
danielaczarref <daniela.czarref@gmail.com>
2024-09-12 16:29:48 -0300

    forgot menu data to commit

---
+++ src/pages/Menu/MenuData.js
@@ -0,0 +1,89 @@
+export const starters = [
+    {
+      id: 1,
+      title: "Greek salad",
+      description:
+        "The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons. ",
+      image:
+        "https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2018/08/Greek-Salad-main.jpg",
+      price: "$12.99",
+    },
+    {
+      id: 2,
+      title: "Bruschetta",
+      description:
+        "Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil.",
+      image:
+        "https://hips.hearstapps.com/hmg-prod/images/bruschetta-index-645d03e6f174d.jpg?crop=0.8890414878397711xw:1xh;center,top&resize=1200:*",
+      price: "$ 5.99",
+    },
+    {
+      id: 3,
+      title: "Steak Tartare",
+      description:
+        "Steak Tartare is a raw preparation of beef that is common in French bistros, brasseries, and cafes. It's best with a side of fries, a light salad, and/or toast points.",
+      image:
+        "https://www.eatthis.com/wp-content/uploads/sites/4/2022/11/Steak-Tartare.jpg?quality=82&strip=all",
+      price: "$ 10.00",
+    },
+  ];
+
+  export const mainCourse = [
+    {
+      id: 1,
+      title: "Molasses-Soy Glazed Salmon and Vegetables",
+      description:
+        "Delicious salmon fillets with brown rice, toasted sesame seeds, minced garlic, yellow bell pepers, soy sauce and lemon slice.",
+      image:
+        "https://www.southernliving.com/thmb/XIHmbOagRrWxoOOrYnQih5VEV9A=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/2548301_QFSHe_646_0_0_0-fbd2a27e126942c8b33dbc3d696b0501.jpg",
+      price: "$32.99",
+    },
+    {
+      id: 2,
+      title: "T-Bone steak",
+      description:
+        "Tasty garlic and rosemary T-bone steak with warm potato salad, pitted Kalamata olives, lemon juice and rocket.",
+      image:
+        "https://img.taste.com.au/NmcRS-oj/taste/2016/11/garlic-and-rosemary-t-bone-steaks-with-warm-potato-salad-102927-1.jpeg",
+      price: "$ 30",
+    },
+    {
+      id: 3,
+      title: "Chicken risotto",
+      description:
+        "Chicken risotto with pumpkin, walnuts, chives, zucchini and country seasoning.",
+      image:
+        "https://marleyspoon.com/media/recipes/114255/main_photos/large/easy_baked_chicken_pumpkin_risotto-8b20b48d4ada86f79ec2fdb2e878975a.jpeg",
+      price: "$ 23.00",
+    },
+  ];
+
+  export const desserts = [
+    {
+      id: 1,
+      title: "Red Wine Pears with Ice-Cream",
+      description:
+        "Anjou pears soaked in the red wine syrup and served with ice cream.",
+      image:
+        "https://www.foodandwine.com/thmb/b-E57jtGU9Q9HYXrF9_UaKvGfxs=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/pears-poached-in-red-wine-FT-RECIPE1021-2-3d3b48514dc448189bf5c92d7cf51d2b.jpg",
+      price: "$8.99",
+    },
+    {
+      id: 2,
+      title: "Lemon Dessert",
+      description:
+        "This comes straight from grandma’s recipe book, every last ingredient has been sourced and is as authentic as can be imagined.",
+      image:
+        "https://www.southernliving.com/thmb/z706skTv8rLcnFwyTEnMzr_H5zQ=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Southern-Living_27364_SR_Lemon-Lush_13190-fd96c709fc2946a7aeb5c869f9ad470e.jpg",
+      price: "$ 5.99",
+    },
+    {
+      id: 3,
+      title: "Tiramisu",
+      description:
+        "Tiramisu, with its combination of sweet mascarpone, bitter coffee and crumbling biscuits, is the perfect pick-me-up dessert",
+      image:
+        "https://www.flavoursholidays.co.uk/wp-content/uploads/2020/07/Tiramisu.jpg",
+      price: "$ 7.80",
+    },
+  ];
